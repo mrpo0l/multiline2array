32f3c9e88d859e88e80fa3c2d17bb4f706909f31
JuanDa <fretesjuan4@gmail.com>
2019-01-13 22:47:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-$('#go').click(function () {
+$('#comvertir').click(function () {
   var lines = $('#input').val().split(/\n/);
   var output = [];
   var outputText = [];
   for (var i = 0; i < lines.length; i++) {
-    // only push this line if it contains a non whitespace character.
     if (/\S/.test(lines[i])) {
       outputText.push("'" + $.trim(lines[i]) + "'");
       output.push($.trim(lines[i]));
